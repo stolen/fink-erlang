e67874aba7a9ce72ded39b1809cb92d8f843c842
Roman Gladkov <d1fffuz0r@gmail.com>
2014-08-23 15:22:10 -0700

    temp

--- src/n2o_fink.erl
+++ src/n2o_fink.erl
@@ -1,1 +1,7 @@
 -module(n2o_fink).
+-include_lib("n2o/include/wf.hrl").
+-compile(export_all).
+-export(?FAULTER_API).
+
+stack() -> n2o_error:stack().
+error_page(Class, Error) -> n2o_error:error_page(Class, Error).

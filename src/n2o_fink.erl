5f7cb6d6dfa6e29ce3ce101fb0f247b344059ee0
Roman Gladkov <d1fffuz0r@gmail.com>
2014-08-23 19:14:31 -0700

    fix

--- src/n2o_fink.erl
+++ src/n2o_fink.erl
@@ -4,9 +4,9 @@
 -export(?FAULTER_API).
 
 stack(Error, Reason) ->
-    fink:n2o_push(Error, Reason),
+    fink:push(Error, Reason),
     n2o_error:stack(Error, Reason).
 
 error_page(Class, Error) ->
-    fink:n2o_push(Class, Error),
+    fink:push(Class, Error),
     n2o_error:error_page(Class, Error).
